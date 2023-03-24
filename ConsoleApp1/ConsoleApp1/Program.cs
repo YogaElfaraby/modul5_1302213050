c54d82a2cb15f99ad74baf96b0aee9bb48e33b80
PC7-IFLAB <yoga85ananta@gmail.com>
2023-03-24 08:57:42 +0700

    menambahkan class SimpleDataBase

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,37 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+           SimpleDataBase<long> wiliam = new SimpleDataBase<long>();
+           wiliam.addnewdata(13);
+           wiliam.addnewdata(02);
+           wiliam.addnewdata(21);
+        }
+
+        public class SimpleDataBase<T>
+        {
+            private List<T> storedData;
+            private List<DateTime> inputDates;
+
+            public SimpleDataBase()
+            {
+                storedData = new List<T>();
+                inputDates = new List<DateTime>();
+            }
+            
+            public void addnewdata(T T)  
+            {
+                storedData.Add(T);
+                inputDates.Add(DateTime.Now);
+            }
+
+            public void PrintAllData()
+            {
+                for(int i = 0; i < storedData.Count; i++)
+                {
+                    Console.WriteLine("Data " + (i + 1) + "berisi: " + storedData[i] + " yang disimpan pada waktu" + inputDates[i]);
+                }
+                
+            }
         }
     }
 }
